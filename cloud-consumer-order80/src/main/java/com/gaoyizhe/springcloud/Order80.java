484d7662e75ca57349b6e5c5b8610251fc573926
gaoyizhe <793431824@qq.com>
2020-05-26 21:29:16 +0800

    消费者模块、公共模块

---
+++ cloud-consumer-order80/src/main/java/com/gaoyizhe/springcloud/Order80.java
@@ -0,0 +1,14 @@
+package com.gaoyizhe.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 订单主类
+ */
+@SpringBootApplication
+public class Order80 {
+    public static void main(String[] args) {
+        SpringApplication.run(Order80.class);
+    }
+}
