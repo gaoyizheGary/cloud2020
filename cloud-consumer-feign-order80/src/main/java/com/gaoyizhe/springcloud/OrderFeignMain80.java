51050028aa5d70b3ed419de7a3cdaa190434b56b
gaoyizhe <793431824@qq.com>
2020-07-20 22:24:26 +0800

    getEntity

---
+++ cloud-consumer-feign-order80/src/main/java/com/gaoyizhe/springcloud/OrderFeignMain80.java
@@ -0,0 +1,18 @@
+package com.gaoyizhe.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+/**
+ * 订单主类
+ */
+@SpringBootApplication
+@EnableEurekaClient
+@EnableFeignClients
+public class OrderFeignMain80 {
+    public static void main(String[] args) {
+        SpringApplication.run(OrderFeignMain80.class, args);
+    }
+}
